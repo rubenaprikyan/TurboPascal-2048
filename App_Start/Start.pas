d62950a4206796fc083ddcda7222f7a4b078934b
Ruben Aprikyan <aprikyan98@gmail.com>
2018-03-05 19:38:46 +0400

    Update Start.pas

--- App_Start/Start.pas
+++ App_Start/Start.pas
@@ -1,1 +1,42 @@
 
+procedure Start;
+  BEGIN
+       a:=0;
+       initgraph(a,b,'');
+       repeat
+       language(key);
+       case key of
+       1:begin
+           lang:=true;
+            repeat
+              mainmenu(key1,lang);
+              case key1 of
+              1: game(lang);
+              2: help;
+              end
+            until key1=3;
+         end;
+       2:begin
+           lang:=false;
+           repeat
+                menu(key1,lang);
+              case key1 of
+               1:game(lang,key1);
+                  begin 
+                    case 
+                      1:signin(password,login) end;
+               2:login;
+                  begin
+                   game(lang)
+                  end; 
+               3:help
+              end;
+           until key1=4;
+       end;
+       end;
+      until key=3;
+  END;
+  BEGIN {Main} 
+    start;
+    readkey
+  END.
